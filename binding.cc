b675d88e8b7981169533d1fc0399f47082421b52
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2015-10-16 23:40:24 +0500

    Init project

---
+++ binding.cc
@@ -0,0 +1,69 @@
+#include <node.h>
+#include <v8.h>
+
+extern "C" {
+#include "ws2812-RPi.h" 
+}
+
+using namespace v8;
+
+using v8::Exception;
+using v8::FunctionCallbackInfo;
+using v8::Isolate;
+using v8::Local;
+using v8::Number;
+using v8::Object;
+using v8::String;
+using v8::Value;
+
+void initPWM(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
+  init_pwm();
+}
+
+void disposePWM(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
+  dispose_pwm();
+}
+
+void show(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
+  show();
+}
+
+void setPixel(const FunctionCallbackInfo<Value>& args) {
+  Isolate* isolate = Isolate::GetCurrent();
+  HandleScope scope(isolate);
+
+  if (args.Length() < 4) {
+    isolate->ThrowException(Exception::TypeError(
+          String::NewFromUtf8(isolate, "Wrong number of arguments")));
+    return;
+  }
+
+  if (!args[0]->IsNumber() || !args[1]->IsNumber() ||
+      !args[2]->IsNumber() || !args[3]->IsNumber()) {
+    isolate->ThrowException(Exception::TypeError(
+          String::NewFromUtf8(isolate, "Wrong arguments")));
+    return;
+  }
+
+  int pixel = args[0]->NumberValue();
+  char r = args[1]->NumberValue();
+  char g = args[2]->NumberValue();
+  char b = args[3]->NumberValue();
+
+  setPixelColor(pixel, r, g, b);
+}
+
+void init(Handle<Object> target) {
+  NODE_SET_METHOD(target, "initPWM", initPWM);
+  NODE_SET_METHOD(target, "disposePWM", disposePWM);
+  NODE_SET_METHOD(target, "show", show);
+  NODE_SET_METHOD(target, "setPixel", setPixel);
+}
+
+NODE_MODULE(binding, init);
---
+++ build/Release/.deps/Release/obj.target/binding/binding.o.d
@@ -0,0 +1,16 @@
+cmd_Release/obj.target/binding/binding.o := g++ '-DNODE_GYP_MODULE_NAME=binding' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/eddig/.node-gyp/4.1.1/include/node -I/home/eddig/.node-gyp/4.1.1/src -I/home/eddig/.node-gyp/4.1.1/deps/uv/include -I/home/eddig/.node-gyp/4.1.1/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -MMD -MF ./Release/.deps/Release/obj.target/binding/binding.o.d.raw  -c -o Release/obj.target/binding/binding.o ../binding.cc
+Release/obj.target/binding/binding.o: ../binding.cc \
+ /home/eddig/.node-gyp/4.1.1/include/node/node.h \
+ /home/eddig/.node-gyp/4.1.1/include/node/v8.h \
+ /home/eddig/.node-gyp/4.1.1/include/node/v8-version.h \
+ /home/eddig/.node-gyp/4.1.1/include/node/v8config.h \
+ /home/eddig/.node-gyp/4.1.1/include/node/node_version.h \
+ /home/eddig/.node-gyp/4.1.1/include/node/v8.h ../ws2812-RPi.h
+../binding.cc:
+/home/eddig/.node-gyp/4.1.1/include/node/node.h:
+/home/eddig/.node-gyp/4.1.1/include/node/v8.h:
+/home/eddig/.node-gyp/4.1.1/include/node/v8-version.h:
+/home/eddig/.node-gyp/4.1.1/include/node/v8config.h:
+/home/eddig/.node-gyp/4.1.1/include/node/node_version.h:
+/home/eddig/.node-gyp/4.1.1/include/node/v8.h:
+../ws2812-RPi.h:
